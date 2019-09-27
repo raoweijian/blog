506f1ea1ec6eb6c8fdf4c84d64707482a57d1420
raoweijian <weijian.rwj@mybank.cn>
2019-09-27 13:01:08 +0800

    a

---
+++ app/static/js/moon.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+    el: "#container",
+    delimiters: ['[[', ']]'],
+    data: {
+        starNum: 250,
+        width: 1,
+        cols: ['#f5d76e', '#f7ca18', '#f4d03f', '#ececec', '#ecf0f1', '#a2ded0'],
+        clazz: ["starFlick", 'starFlick2', 'starFlick3', 'starFlick4'],
+    },
+
+    created: function(){
+    },
+
+    mounted: function(){},
+
+    methods: {
+    },
+
+    watch: {},
+});
